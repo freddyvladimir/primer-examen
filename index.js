f899ad6bce3cdb49ebfcc770b71736009fd95108
vladi <elnuevovirus2@gmail.com>
2024-05-13 19:48:21 -0400

    inicio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +11,7 @@ const routesCalificacion = require('./routes/calificacionRoutes');
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect(MONGO_URI).then(
     () => {
         console.log("connection exitosa");
